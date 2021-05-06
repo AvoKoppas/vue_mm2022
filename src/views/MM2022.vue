6793e8bd3fe5e3b8ca6fdfa91aa10e38a9228994
Veiko Ilves <veikoilves10@gmail.com>
2021-05-06 14:46:05 +0300

    comm

--- src/views/MM2022.vue
+++ src/views/MM2022.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="list">
-    <h1>Kõik kontod</h1>
-    <button v-on:click="getAllBalance()">Kõik kontod</button>
-    <h1 align="left">Nimekiri kontodest</h1>
+    <h1>Edetabel</h1>
+    <!--        <input v-model.number="score">
+            <input v-model="user_name">-->
+    <button v-on:click="gameScore()">Refresh</button>
     <table>
       <tr>
-        <th>Account Number</th>
-        <th align="left">Balance</th>
+        <th>Kasutaja</th>
+        <th align="left">Punktid</th>
       </tr>
-      <tr v-for="account in allBalanceAnswer">
-        <td align="left">{{ account.accountNr }}</td>
-        <td align="left">{{ account.balance + "€" }}</td>
+      <tr v-for="userName in scoreTable" v-bind:key="userName.userName">
+        <td align="left">{{ userName.userName }}</td>
+        <td align="left">{{ userName.score }}</td>
       </tr>
     </table>
+
   </div>
 </template>
 
@@ -20,20 +22,59 @@
 export default {
   data: function () {
     return {
-      'allBalanceAnswer': '',
-      'accountNumber': ''
+      'score': '',
+      'user': '',
+      'scoreTable': ''
     }
+
   },
   methods: {
-    'getAllBalance': function () {
-      this.$http.get('http://localhost:8080/getAllBalance/')
+    'gameScore': function () {
+      this.$http.get('http://localhost:8080/scoreList/')
           .then(response => {
             console.log(response);
-            this.allBalanceAnswer = response.data
+            this.scoreTable = response.data
           })
     }
   }
+
 }
+
+
+/*</table>
+<button v-on:click="getAllScores()">Refresh</button>
+<h1 align="left">Nimekiri kontodest</h1>
+<table>
+  <tr>
+    <th>Account Number</th>
+    <th align="left">Balance</th>
+  </tr>
+  <tr v-for="account in allBalanceAnswer">
+    <td align="left">{{ account.accountNr }}</td>
+    <td align="left">{{ account.balance + "€" }}</td>
+  </tr>
+</table>
+</div>
+</template>
+
+
+export default {
+data: function () {
+return {
+  'allBalanceAnswer': '',
+  'accountNumber': ''
+}
+},
+methods: {
+'getAllBalance': function () {
+  this.$http.get('http://localhost:8080/getAllBalance/')
+      .then(response => {
+        console.log(response);
+        this.allBalanceAnswer = response.data
+      })
+}
+}-->
+}*/
 </script>
 
 <style scoped>
